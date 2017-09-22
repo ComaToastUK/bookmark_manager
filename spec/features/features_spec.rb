7a3ae59b70d3e960c0515d18ee77357f3198ea4d
ComaToastUK <jesusmanyouatethedog@gmail.com>
2017-09-22 20:47:18 +0100

    fixed a few bugs

--- spec/features/features_spec.rb
+++ spec/features/features_spec.rb
@@ -1,34 +1,42 @@
 feature 'List of links' do
-  scenario 'viewing the list on the homepage' do
+
+  let!(:user) do
+  User.create(email: 'james@example.com',
+              password: 'password',
+              password_confirmation: 'password')
+            end
+    scenario 'viewing the list on the homepage' do
+    sign_in(email: user.email,   password: user.password)
     Link.create(url: 'https://www.reddit.com', title: 'Reddit')
 
     visit '/links'
     expect(page.status_code).to eq 200
-
-    within 'ul#links' do
       expect(page).to have_content('www.reddit.com')
     end
   end
-end
 
 # As a time-pressed user
 # So that I can save a website
 # I would like to add the site's address and title to my bookmark manager
 
 feature 'adding links to the bookmark manager' do
+  let!(:user) do
+  User.create(email: 'james@example.com',
+              password: 'password',
+              password_confirmation: 'password')
+            end
   scenario 'adding www.bbc.co.uk/news to the bookmark manager' do
+
+    sign_in(email: user.email,   password: user.password)
     visit '/links/new'
     fill_in 'url',   with: 'http://www.bbc.com/'
     fill_in 'title', with: 'BBC Homepage'
     click_button 'Create link'
 
-    expect(current_path).to eq '/links'
-
-    within 'ul#links' do
+      expect(current_path).to eq '/links'
       expect(page).to have_content('BBC Homepage')
     end
   end
-end
 
 # As a time-pressed user
 # So that I can organise my many links into different categories for ease of search
